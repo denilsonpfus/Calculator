b8a37c21f9d9c2efc8db84b9337896694f443ad7
kpx314159 <kpx314159@gmail.com>
2017-11-30 15:23:53 -0800

    Chnages

--- Dockerfile
+++ Dockerfile
@@ -1,23 +1,18 @@
-FROM node:9.1.0
+# pull lean version of NODE running on Alpine LINUX
+#FROM node:9.2.0-alpine
+FROM mhart/alpine-node:0.10
 
-# Create app directory
+# Create directory inside the container
 WORKDIR /usr/src/app
 
-# Install app dependencies
-# A wildcard is used to ensure both package.json AND package-lock.json are copied
-# where available (npm@5+)
-COPY package*.json ./
+# Copy package.json for NPM to run the build script and evaluation.js (program) to create the image
+COPY package.json ./
+COPY Evaluation.js ./
 
-RUN npm install
-RUN npm install eval
-
-
-# Bundle app source
-COPY . .
-
+# install npm; assign port 9000 to container; run command npm start to run script in package.json tagged with start to evalauation.js
+#RUN npm i express body-parser cors eval 
+RUN npm install --production
 EXPOSE 9000
-
-# CMD ["node","Calculator.js"]
 CMD ["npm","start"]
 
 
--- Evaluation.js
+++ Evaluation.js
@@ -1,48 +1,31 @@
 var express = require('express');
-//var path = require('path');
-var http = require('http');
 var app = express();
-//var cors = require('cors');
 
-//app.use(cors({origin: 'null'}));
-app.use(function (req, res, next) {
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:9000');
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:32770');
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Origin', 'null');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-    res.setHeader('Access-Control-Allow-Credentials', true);
-    next();
-});
+var cors = require('cors');
+app.use(cors());
 
-
-
-// Define the port to run on
+// indicate port for EXPRESS to listen to...
 app.listen(9000);
-console.log('Listening to port 9000');
-var pho = eval('3+5');
-console.log(pho);
 
-// Entry point - index.html landing page
-//app.use(express.static(path.join(__dirname)));
+// test data
+console.log('Listening to port 9000'); var pho = eval('3+5'); console.log('Test ----> ' + pho +  '\nCALCULATOR ON!!!!');
 
-//Listen for requests
-app.get('/', function(req, res){
-    res.type('text/plain');
-//    res.send('55555');
-//http.createServer(function(req, res){
-//    var eval_data = eval('3+5');
-//var x = req;
-//var y = '2';
-//var eval_data = x + y;
-    req.on('data', function (data) {
-//       eval_data = eval('3+5');
-//     var equals = eval(req);
-        res.send('1'+ data);
+// '/xxx is the end point from client javascript call'
+app.post('/', function(req, res){
+//   res.type('text/plain');
+var data = '';
+    req.on('data', function (chain) {
+        data += chain;
     });
+
     req.on('end', function() {
-        res.send('2' + data);
+        var result = eval(data);
+
+  // EXPRESS does NOT accept integers. ONLY: Buffer object, Object, Array, or String
+        res.send(String(result));
+        console.log(result);
+        // kill EXPRESS after one operation
+  //      app.close();
       });
 });
     
